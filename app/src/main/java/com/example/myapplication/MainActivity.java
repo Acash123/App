010f3c219dae5f56d68413a14dedc37c9446e904
Acash123 <akashtharuka@ymail.com>
2019-03-11 09:20:30 +0530

    Implemented a Handler

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,13 +6,26 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int counter = 100;
+    private int countDown = 99;
+    private static final String CURRENT_COUNTER = "countDown" ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            countDown = savedInstanceState.getInt(CURRENT_COUNTER);
+        }
+
         CountDown();
+
     }
+    // 11-03-2019
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState );
+        savedInstanceState.putInt(CURRENT_COUNTER,countDown);
+    }
+
     private void CountDown(){
         final TextView textView=findViewById(R.id.textView);
       //  textView.setText("Hello"); Use this to Check for initial checking
@@ -20,9 +33,15 @@ public class MainActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                textView.setText(Integer.toString(counter));
-                counter--;
+                textView.setText(Integer.toString(countDown));
+               // If u put the if condition in here U'll have to replace -1 with 0
+                countDown--;
                 handler.postDelayed(this, 1000);
+                //---
+                if(countDown==-1){
+                    countDown=100; // Resetting the Counter
+                }
+
 
             }
         });
